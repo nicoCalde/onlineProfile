7e681166885d1991acea0167c89cec3395c8b31e
nicoCalde <nicogonzalezcalderon@gmail.com>
2024-03-13 18:23:46 -0300

    solved main.js error

--- opApp/static/opApp/js/main.js
+++ opApp/static/opApp/js/main.js
@@ -63,69 +63,72 @@ document.addEventListener("DOMContentLoaded", function() {
     const messageForm = document.querySelector('.message-form');
     const messageInput = document.querySelector('.message-input');
 
-    messageForm.addEventListener('submit', (event) => {
-        event.preventDefault();
+    if (messageForm) {
+        messageForm.addEventListener('submit', (event) => {
+            event.preventDefault();
 
-        const message = messageInput.value.trim();
-        if (message.length === 0) {
-            return;
-        }
+            const message = messageInput.value.trim();
+            if (message.length === 0) {
+                return;
+            }
 
-        const messageItem = document.createElement('li');
-        messageItem.classList.add('message', 'sent');
-        messageItem.innerHTML = `
-            <div class="message-text user">
-                <div class="message-sender">
-                    <b>You</b>
-                </div>
-                <div class="message-content">
-                    ${message}
-                </div>
-            </div>`;
-        messagesList.appendChild(messageItem);
-
-        messageInput.value = '';
-
-        fetch('/opApp/chatbot/', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: new URLSearchParams({
-                'csrfmiddlewaretoken': document.querySelector('[name=csrfmiddlewaretoken]').value,
-                'message': message
-            })
-        })
-        .then(response => response.json())
-        .then(data => {
-            const response = data.response;
             const messageItem = document.createElement('li');
-            messageItem.classList.add('message', 'received');
+            messageItem.classList.add('message', 'sent');
             messageItem.innerHTML = `
-            <div class="message-text bot">
-                <div class="message-sender">
-                    <b>nicoAI</b>
-                </div>
-                <div class="message-content">
-                    ${response}
-                </div>
-            </div>
-                `;
+                <div class="message-text user">
+                    <div class="message-sender">
+                        <b>You</b>
+                    </div>
+                    <div class="message-content">
+                        ${message}
+                    </div>
+                </div>`;
             messagesList.appendChild(messageItem);
-        });
-    });
 
+            messageInput.value = '';
+
+            fetch('/opApp/chatbot/', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                body: new URLSearchParams({
+                    'csrfmiddlewaretoken': document.querySelector('[name=csrfmiddlewaretoken]').value,
+                    'message': message
+                })
+            })
+            .then(response => response.json())
+            .then(data => {
+                const response = data.response;
+                const messageItem = document.createElement('li');
+                messageItem.classList.add('message', 'received');
+                messageItem.innerHTML = `
+                <div class="message-text bot">
+                    <div class="message-sender">
+                        <b>nicoAI</b>
+                    </div>
+                    <div class="message-content">
+                        ${response}
+                    </div>
+                </div>
+                    `;
+                messagesList.appendChild(messageItem);
+            });
+        });
+    }
     const activateChatbotBtn = document.querySelector('.activate-chatbot');
     const closeIcon = document.querySelector('.close-icon');
     const chatContainer = document.querySelector('.chat-container');
 
-    activateChatbotBtn.addEventListener('click', function() {
-        if (chatContainer.style.display === 'block') {
-            chatContainer.style.display = 'none';
-        } else {
-            chatContainer.style.display = 'block';
-        }
-    });
+    if (activateChatbotBtn) {
+        activateChatbotBtn.addEventListener('click', function() {
+            if (chatContainer.style.display === 'block') {
+                chatContainer.style.display = 'none';
+            } else {
+                chatContainer.style.display = 'block';
+            }
+        });
 
-    closeIcon.addEventListener('click', function() {
-        chatContainer.style.display = 'none';
-    });
+        closeIcon.addEventListener('click', function() {
+            chatContainer.style.display = 'none';
+        });
+    }
 });
